bcd807c81d824e833bc6e9817ce60882afb90537
JulioVanini <70251486+JulioVanini@users.noreply.github.com>
2020-08-26 22:42:48 -0500

    Update 04-calculadora.R
    Un par de comentarios complementarios

--- scripts/tema1/04-calculadora.R
+++ scripts/tema1/04-calculadora.R
@@ -31,7 +31,8 @@ pi
 
 pi^2
 
-Inf
+Inf 
+
 
 -Inf
 
@@ -44,7 +45,7 @@ NaN
 0/0
 
 2^50# = 1.1259e+15 = 1125900000000000 
-
+0
 2 ^(-15) # 3.051758e-05 = 0.00003051758
 
 c(2^30, 2^(-15), 1, 2, 3/2)
@@ -55,6 +56,8 @@ sqrt(25)
 exp(1)
 
 log(pi)
+
+abs(-pi) 
 
 log(32, 2)
 
@@ -93,25 +96,25 @@ asin(0.8660254) #arc sin en radianes
 
 asin(0.8660254) * 180 /pi #arc sin en grados
 
-asin(5) #arc sin x in [-1,1]
+ asin(5) #arc sin x in [-1,1]
 
 acos(-8)
-
-print(sqrt(2),10)
-round(sqrt(2), 3)
+sqrt(2)
+print(sqrt(2),10) #indica cuantas cifras va a mostrar 
+round(sqrt(2), 3) #redondear despues de las cifras indicadas
 floor(sqrt(2))
 ceiling(sqrt(2))
 trunc(sqrt(2))
 
-sqrt(2)^2-2
+floor(sqrt(2)^2-2)
 
 round(sqrt(2), 4)^2
 
 2^50
 print(2^50, 15)
 print(2^50, 2)
-print(pi, 22)
-#3.141592653589793115998
+print(pi, 13)
+#3.141592653589793115998 #Despues de 16 dijisto R tiende a ser impreciso
 #3.141592653589793238462
 
 round(1.25,1)
@@ -120,7 +123,7 @@ round(sqrt(2),0)
 round(sqrt(2))
 
 round(digits = 5, sqrt(2))
-round(5, sqrt(2))
+  round(5, sqrt(2))
 
 
 floor(-3.45)
@@ -139,7 +142,7 @@ z
 
 edad <- 30
 
-nombre = "Juan Gabriel"
+nombre = "Julio Cesar"
 
 HOLA = 1
 hola = 5
@@ -147,7 +150,7 @@ hola = 5
 pi.4 = 4*pi
 pi.4
 
-x = 2
+ x = 2
 x = x^2
 
 x = sqrt(x)
